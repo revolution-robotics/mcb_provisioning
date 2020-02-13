2ad44194ce5071b68ba8b6a3cf3cdd75112cb857
Jacob Postman <jacob@revolution-robotics.com>
2020-02-12 17:14:23 -0800

    Add error checking and user prompts
    Require input argument for target block device
    Check for and exit on errors on each step
    Clean up previous attempts before running:
    - Attempt to unmount mount points
    - Remove logical volumes
    - Remove volume groups
    - Remove physical volume
    - Remove stale dev mappings
    - Remove /etc/lvm/backup file
    Show progress during dd and xfsdump
    Generate a new UUID for network interfaces
    Unmount when complete

--- install_miw_os.sh
+++ install_miw_os.sh
@@ -13,8 +13,6 @@
 # /etc/default/grub - change centos_unassigned to centos_mcb (or other lvm vg name)
 # /boot/grub2/grub.cfg - change centos_unassigned to centos_mcb (or other lvm vg name)
 
-readonly TARGET_BLOCK_DEV="/dev/sdb"
-
 readonly COMBINED_MBR_BOOT_PARTITION_IMAGE="6FEB2020_miw_boot_part_only.img"
 # miw_boot_part_only.img was created using dd command after using fdisk to delete partition 2
 # sudo dd if=/dev/sdb of=6FEB2020_miw_boot_part_only.img bs=512 count=2099199
@@ -24,17 +22,99 @@ readonly COMBINED_MBR_BOOT_PARTITION_IMAGE="6FEB2020_miw_boot_part_only.img"
 # the second Linux LVM partition can be added using the fdisk command in this script, allowing the
 # LVM partition to be dynamically sized to the full size of the disk.
 
+readonly LVM_VG_NAME=centos_mcb
+
 # Validate input argument
-# TODO
+
+TARGET_BLOCK_DEV=$1
+MAP_LOCATION=$(echo ${TARGET_BLOCK_DEV} | sed 's/dev\//dev\/mapper\//')
+
+# Check for input argument
+if [ -z ${TARGET_BLOCK_DEV} ]; then
+    echo "A block device is required as input argument."
+    exit 1
+fi
+
+# Check if target is a block device, warn and exit if not
+if [ ! -b ${TARGET_BLOCK_DEV} ]; then
+    echo "Target must be a block device: ${TARGET_BLOCK_DEV}"
+    exit 1
+fi
+
+# Warn user that data on the target device will be destroyed.
+read -r -p "WARNING: ALL DATA ON ${TARGET_BLOCK_DEV} WILL BE DESTROYED. Are you sure you want to continue? [Y/n]" input
+case $input in [yY][eE][sS]|[yY])
+    echo "Good luck! ;)"
+    ;;
+*)
+    echo "Exiting."
+    exit 1
+    ;;
+esac
+
+echo "Unmount target disk mount points if mounted"
+umount /mnt/home
+umount /mnt/root
+umount /mnt/mcb
+
+lvremove ${LVM_VG_NAME} 
+vgremove ${LVM_VG_NAME}
+pvremove ${TARGET_BLOCK_DEV}2
+
+echo "Removing stale dev mapping for ${MAP_LOCATION}1"
+dmsetup remove ${MAP_LOCATION}1
+echo "Removing stale dev mapping for ${MAP_LOCATION}2"
+dmsetup remove ${MAP_LOCATION}2
+
+kpartx -u ${TARGET_BLOCK_DEV}
+
+# Remove /etc/lvm/backup/${LVM_VG_NAME}
+if [ -f /etc/lvm/backup/${LVM_VG_NAME} ]; then
+    read -r -p "/etc/lvm/backup/${LVM_VG_NAME} already exists. Remove it? [Y/n]" input
+    case $input in [yY][eE][sS]|[yY])
+        echo "Deleting /etc/lvm/backup/${LVM_VG_NAME}"
+        rm /etc/lvm/backup/${LVM_VG_NAME} || {
+            echo "Error($?) on line#: $LINENO. Failed to delete /etc/lvm/backup/${LVM_VG_NAME}. Exiting..."
+            exit 1
+        }
+        ;;
+    *)
+        echo "Can not continue while lvm backup exists."
+        exit 1
+        ;;
+    esac
+fi
+
+# sync
+# partprobe
+# kpartx -u ${TARGET_BLOCK_DEV}
+# sync
 
 # Clear existing partition table
-dd if=/dev/zero of=${TARGET_BLOCK_DEV} bs=512 count=512
+echo "Zero'ing out the partition table"
+dd if=/dev/zero of=${TARGET_BLOCK_DEV} bs=512 count=512 status=progress || {
+    echo "Error($?) on line#: $LINENO. Failed to zero out partition table on ${TARGET_BLOCK_DEV}. Exiting..."
+    exit 1
+}
+# sync
+# partprobe
+kpartx -u ${TARGET_BLOCK_DEV}
+sync
 
 # Write partition table, grub, and boot partition from image
 # Partition table at this point only includes a 1GB boot partition
 # LVM Partition will be added to fill the remaining disk space using fdisk
-sudo dd if=${COMBINED_MBR_BOOT_PARTITION_IMAGE} of=${TARGET_BLOCK_DEV}
+echo "Write MBR and boot partition"
+dd if=${COMBINED_MBR_BOOT_PARTITION_IMAGE} of=${TARGET_BLOCK_DEV} status=progress || {
+    echo "Error($?) on line#: $LINENO. Failed during dd of MBR and BOOT partition. Exiting..."
+    exit 1
+}
+sync
+partprobe
+kpartx -u ${TARGET_BLOCK_DEV}
+sync
 
+echo "Adding LVM partition to ${TARGET_BLOCK_DEV}"
 # Partition drive
 fdisk ${TARGET_BLOCK_DEV} << EOF
 n
@@ -49,37 +129,136 @@ p
 w
 EOF
 
+retVal=$?
+echo "Return code of fdisk command: ($retVal)"
+if [ $retVal -ne 0 ]; then
+    echo "Error($retVal) during fdisk command. Exiting..."
+    exit 1
+fi
+# echo "Sleeping for 2s"
+# sleep 2
+# sync
+# echo "Sleeping for 10s"
+# sleep 10
+echo "Removing stale dev mapping for ${MAP_LOCATION}1"
+dmsetup remove ${MAP_LOCATION}1
+echo "Removing stale dev mapping for ${MAP_LOCATION}2"
+dmsetup remove ${MAP_LOCATION}2
+
 # If this disk has been used before on this machine there may be cached versions of the lvm volumes
 # that will prevent the creation of new pv vg and lv commands. These can be removed in the
 # /etc/lvm/backup and /etc/lvm/archive directories
 
 ##### Configure LVM volumes #####
 # Create physical volume
-pvcreate ${TARGET_BLOCK_DEV}2
+pvcreate ${TARGET_BLOCK_DEV}2 || {
+    echo "Error($?) on line#: $LINENO while creating physical volume. Exiting..."
+    exit 1
+}
 
 # Create volume group
-vgcreate centos_mcb ${TARGET_BLOCK_DEV}2
+vgcreate ${LVM_VG_NAME} ${TARGET_BLOCK_DEV}2 || {
+    echo "Error($?) on line#: $LINENO while creating volume group. Exiting..."
+    exit 1
+}
 
 # Create logical volumes
-lvcreate -L 16G -n swap centos_mcb
-lvcreate -L 20G -n home centos_mcb
-lvcreate -L 50G -n root centos_mcb
-lvcreate -L 840G -n mcb centos_mcb
+lvcreate -L 16G -n swap ${LVM_VG_NAME} || {
+    echo "Error($?) on line#: $LINENO. Exiting..."
+    exit 1
+}
 
-mkswap /dev/centos_mcb/swap
-mkfs.xfs /dev/centos_mcb/home
-mkfs.xfs /dev/centos_mcb/root
-mkfs.xfs /dev/centos_mcb/mcb
+lvcreate -L 20G -n home ${LVM_VG_NAME} || {
+    echo "Error($?) on line#: $LINENO. Exiting..."
+    exit 1
+}
 
-mkdir /mnt/home
-mkdir /mnt/root
-mkdir /mnt/mcb
+lvcreate -L 50G -n root ${LVM_VG_NAME} || {
+    echo "Error($?) on line#: $LINENO. Exiting..."
+    exit 1
+}
 
-mount /dev/centos_mcb/home /mnt/home/
-mount /dev/centos_mcb/root /mnt/root/
-mount /dev/centos_mcb/mcb /mnt/mcb
+lvcreate -l +99%FREE -n mcb ${LVM_VG_NAME} || {
+    echo "Error($?) on line#: $LINENO. Exiting..."
+    exit 1
+}
 
-xfsrestore -f home_lvm.image /mnt/home/
-xfsrestore -f root_lvm.image.v2 /mnt/root/
-xfsrestore -f mcb_lvm.image /mnt/mcb/
+mkswap /dev/${LVM_VG_NAME}/swap || {
+    echo "Error($?) on line#: $LINENO. Exiting..."
+    exit 1
+}
+mkfs.xfs /dev/${LVM_VG_NAME}/home || {
+    echo "Error($?) on line#: $LINENO. Exiting..."
+    exit 1
+}
+mkfs.xfs /dev/${LVM_VG_NAME}/root || {
+    echo "Error($?) on line#: $LINENO. Exiting..."
+    exit 1
+}
+mkfs.xfs /dev/${LVM_VG_NAME}/mcb || {
+    echo "Error($?) on line#: $LINENO. Exiting..."
+    exit 1
+}
 
+echo "Creating mount points if they do not already exist:"
+echo "Creating /mnt/home"
+mkdir -p /mnt/home
+echo "Creating /mnt/root"
+mkdir -p /mnt/root
+echo "Creating /mnt/mcb"
+mkdir -p /mnt/mcb
+
+echo "Mounting /dev/${LVM_VG_NAME}/home to /mnt/home"
+mount /dev/${LVM_VG_NAME}/home /mnt/home/ || {
+    echo "Error($?) on line#: $LINENO. Exiting..."
+    exit 1
+}
+
+echo "Mounting /dev/${LVM_VG_NAME}/root to /mnt/root"
+mount /dev/${LVM_VG_NAME}/root /mnt/root/ || {
+    echo "Error($?) on line#: $LINENO. Exiting..."
+    exit 1
+}
+
+echo "Mounting /dev/${LVM_VG_NAME}/mcb to /mnt/mcb"
+mount /dev/${LVM_VG_NAME}/mcb /mnt/mcb || {
+    echo "Error($?) on line#: $LINENO. Exiting..."
+    exit 1
+}
+
+echo "Restoring xfsdump home_lvm.image to /mnt/home"
+xfsrestore -p 5 -f home_lvm.image /mnt/home/ || {
+    echo "Error($?) on line#: $LINENO. Exiting..."
+    exit 1
+}
+
+echo "Restoring xfsdump root_lvm.image to /mnt/root"
+xfsrestore -p 5 -f root_lvm.image /mnt/root/ || {
+    echo "Error($?) on line#: $LINENO. Exiting..."
+    exit 1
+}
+
+echo "Restoring xfsdump mcb_lvm.image to /mnt/mcb"
+xfsrestore -p 5 -f mcb_lvm.image /mnt/mcb/ || {
+    echo "Error($?) on line#: $LINENO. Exiting..."
+    exit 1
+}
+
+NEW_UUID1=$(uuidgen)
+echo "Updating eno2 ethernet interface UUID: ${NEW_UUID1}"
+sed -i -e "s/e3930085-af04-4a52-9328-9e4565166dbb/${NEW_UUID1}/" /mnt/root/etc/sysconfig/network-scripts/ifcfg-eno1 || {
+    echo "Error($?) on line#: $LINENO. Exiting..."
+    exit 1
+}
+
+NEW_UUID2=$(uuidgen)
+echo "Updating eno2 ethernet interface UUID: ${NEW_UUID2}"
+sed -i -e "s/0f8926bd-cbd7-412d-8855-abe33e1f8177/${NEW_UUID2}/" /mnt/root/etc/sysconfig/network-scripts/ifcfg-eno2 || {
+    echo "Error($?) on line#: $LINENO. Exiting..."
+    exit 1
+}
+
+echo "Unmounting target disk mount points"
+umount /mnt/home
+umount /mnt/root
+umount /mnt/mcb
